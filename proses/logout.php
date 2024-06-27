7907bebd6d57c6999d74e07e2c5f6bfd50c5408c
AbrarAbe <abrar.abe01@gmail.com>
2024-06-27 11:38:25 +0700

    Logout unset session

---
+++ proses/logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (isset($_SESSION['id_user']) && $_SESSION['role'] == true) {
+    unset($_SESSION['role']);
+    unset($_SESSION['id_user']);
+    session_destroy();
+    header('Location: ../index.php');
+} else {
+    header('Location: ../index.php');
+}
+?>
